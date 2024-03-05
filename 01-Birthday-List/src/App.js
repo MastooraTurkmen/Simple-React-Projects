2f78ab218e53630e7b4f8f1f9899cd623cbf475a
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-05 16:40:34 +0430

    Birthday set up with useState

--- 01-Birthday-List/src/App.js
+++ 01-Birthday-List/src/App.js
@@ -1,8 +1,18 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data)
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} Birthday Today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])} type='button' className='btn'>Clear Item</button>
+      </section>
+    </main>
+  )
 }
 
 export default App;
