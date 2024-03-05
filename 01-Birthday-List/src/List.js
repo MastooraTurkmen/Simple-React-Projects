da8709b3a4319380b47c2047840e95333d8f73cd
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-05 16:40:42 +0430

    Birthday set up with map

--- 01-Birthday-List/src/List.js
+++ 01-Birthday-List/src/List.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
-const List = () => {
+const List = ({ people }) => {
   return (
     <>
-      <h2>list component</h2>
+      {people.map((item) => {
+        const { id, name, age, image } = item;
+        return (
+          <article key={id} className='person'>
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
+          </article>
+        )
+      })}
     </>
   );
 };
