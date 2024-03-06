5ba7ae0c9a7689d455925014009e4fd82e1de672
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-06 15:52:43 +0430

    set up app js

--- 02-Tours/src/App.js
+++ 02-Tours/src/App.js
@@ -4,8 +4,38 @@ import Tours from './Tours'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
+
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([])
+
+  useEffect(async () => {
+    try {
+      const respons = await fetch(url);
+      const tours = await respons.json();
+      setTours(tours)
+      setIsLoading(false)
+
+    } catch (error) {
+      setIsLoading(false)
+      console.log(error);
+    }
+  }, [])
+
+  if (isLoading) {
+    return <div>
+      <Loading />
+    </div>
+  }
+
+  return (
+    <>
+      <section className='container'>
+        <h1>Our tours</h1>
+        <Tours tours={tours} />
+      </section>
+    </>
+  )
 }
 
 export default App
