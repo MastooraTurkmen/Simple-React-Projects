0eaade76c94f0b914317d077e267386d9d1d546c
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-07 08:07:31 +0430

    menu category

--- 05-Menu/src/Categories.js
+++ 05-Menu/src/Categories.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ categories, categoriesItem }) => {
+  return (
+    <div className='btn-container'>
+      {categories.map((category, index) => {
+        return (
+          <button
+            className='filter-btn'
+            type='button'
+            key={index}
+            onClick={() => categoriesItem(category)}
+          >
+            {category}
+          </button>
+        )
+      })}
+    </div>
+  )
 };
 
 export default Categories;
