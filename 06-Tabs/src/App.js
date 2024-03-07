259b425d8083a29f3d22507a1f93d91cc045f896
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-07 09:45:28 +0430

    app js set up for tabs

--- 06-Tabs/src/App.js
+++ 06-Tabs/src/App.js
@@ -1,10 +1,35 @@
 import React, { useState, useEffect } from 'react'
 import { FaAngleDoubleRight } from 'react-icons/fa'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+
+const url = 'https://course-api.com/react-tabs-project  '
+
+
 function App() {
-  return <h2>tabs project setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data)
+    setLoading(false)
+    setJobs(data)
+  }
+
+  useEffect(() => {
+    fetchJobs()
+  }, [])
+
+  if (loading) {
+    return (
+      <section className='section loading'>
+        <h1>loading....</h1>
+      </section>
+    )
+  }
+
+  return <h2>tabs</h2>
 }
 
 export default App
