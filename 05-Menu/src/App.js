554db7888caeb812d72e32f0a714be984784b4c1
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-07 06:47:57 +0430

    set up menu

--- 05-Menu/src/App.js
+++ 05-Menu/src/App.js
@@ -4,7 +4,21 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItem, setMenuItem] = useState(items);
+  const [categories, setCategories] = useState([])
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories />
+        <Menu />
+      </section>
+    </main>
+  )
 }
 
 export default App;
