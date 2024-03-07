b2f8b7d91866f2ae7d8e93f06ab83c1eefc1041f
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-07 08:07:25 +0430

    menu category

--- 05-Menu/src/App.js
+++ 05-Menu/src/App.js
@@ -3,9 +3,23 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+
+const categoriesItems = ['all', ...new Set(items.map((item) =>
+  item.category
+))]
+
 function App() {
   const [menuItem, setMenuItem] = useState(items);
-  const [categories, setCategories] = useState([])
+  const [categories, setCategories] = useState(categoriesItems);
+
+  const categoriesItem = (category) => {
+    if (category === 'all') {
+      setMenuItem(items)
+      return;
+    }
+    const categoryItem = items.filter((item) => item.category === category)
+    setMenuItem(categoryItem)
+  }
 
   return (
     <main>
@@ -14,8 +28,8 @@ function App() {
           <h2>our menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories />
-        <Menu />
+        <Categories categoriesItem={categoriesItem} categories={categories} />
+        <Menu items={menuItem} />
       </section>
     </main>
   )
