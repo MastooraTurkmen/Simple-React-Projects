9017fddce1c8af82dac7f11103e6ae2cc5bec206
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-07 10:15:50 +0430

    button set up

--- 06-Tabs/src/App.js
+++ 06-Tabs/src/App.js
@@ -29,7 +29,48 @@ function App() {
     )
   }
 
-  return <h2>tabs</h2>
+  const { id, title, dates, duties, company } = jobs[value]
+  return (
+    <section className='section'>
+      <div className="title">
+        <h2>expierence</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        {/* btn container */}
+        <div className="btn-container">
+          {
+            jobs.map((item, index) => {
+              return (
+                <button
+                  onClick={() => setValue(index)}
+                  key={item.id}
+                  className={`job-btn ${index === value && 'active-btn'}`}
+                >
+                  {item.company}
+                </button>
+              )
+            })
+          }
+        </div>
+        {/* job info */}
+        <article className='job-info'>
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className='job-date'>{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className='job-icon' />
+                <p>{duty}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+
+    </section>
+  )
 }
 
 export default App
