5807887ae8f9c7298a9939c4196d0b199ac358d8
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-09 10:22:27 +0430

    set Values

--- 09-Color-Generator/src/App.js
+++ 09-Color-Generator/src/App.js
@@ -6,7 +6,7 @@ import Values from 'values.js'
 function App() {
   const [color, setColor] = useState('');
   const [error, setError] = useState(false)
-  const [list, setList] = useState([])
+  const [list, setList] = useState(new Values('#f15025').all(10))
 
   const handleSubmit = (e) => {
     e.preventDefault()
