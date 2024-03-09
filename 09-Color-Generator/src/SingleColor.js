b407b3a49cddd5e9a8e9cfa4d57524589d493cad
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-09 10:35:03 +0430

    set up alert

--- 09-Color-Generator/src/SingleColor.js
+++ 09-Color-Generator/src/SingleColor.js
@@ -4,13 +4,25 @@ import rgbToHex from './utils'
 const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const [alert, setAlert] = useState(false);
   const bcg = rgb.join(',');
-  const hex = rgbToHex(...rgb)
+  // const hex = rgbToHex(...rgb)
   const hexValue = `#${hexColor}`
 
+  useEffect(() => {
+    const time = setTimeout(() => {
+      setAlert(false)
+    }, 3000)
+
+    return () => clearTimeout(time)
+  }, [alert])
+
   return (
-    <article className={`color ${index > 10 && 'color-light'}`} style={{ backgroundColor: `rgb(${bcg})` }}>
+    <article onClick={() => {
+      setAlert(true)
+      navigator.clipboard.writeText(hexValue)
+    }} className={`color ${index > 10 && 'color-light'}`} style={{ backgroundColor: `rgb(${bcg})` }}>
       <p className='precent-value'>{weight}%</p>
       <p className="color-value">{hexValue}</p>
+      {alert && <p className='alert'>copy to clipboard</p>}
     </article>
   )
 }
