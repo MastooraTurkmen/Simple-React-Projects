727fb4f05c573a2965146c325111788fa8fb00ff
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-11 14:49:29 +0430

    set up list and alert

--- 10-Grocery-Bud/src/App.js
+++ 10-Grocery-Bud/src/App.js
@@ -9,7 +9,31 @@ function App() {
   const [edit, setEdit] = useState(null)
   const [alert, setAlert] = useState({ show: false, msg: "", tyep: "" });
 
-
+ 
+  return (
+    <section className='section-center'>
+      <form className="grocery-form" onSubmit={handleSubmite}>
+        {alert.show && <List />}
+        <h3>grocery bud</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className='grocery'
+            placeholder='e.g. milk'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type='submit' className='submit-btn'>
+            {isEditing ? "edit" : "sumbit"}
+          </button>
+        </div>
+      </form>
+      <div className="grocery-container">
+        <List />
+        <button className='clear-btn'>clear item</button>
+      </div>
+    </section>
+  )
 }
 
 export default App
