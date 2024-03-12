5c4c41cf2057a57847a2e214a9216e912201505c
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-12 10:58:25 +0430

    set up buttons

--- 10-Grocery-Bud/src/List.js
+++ 10-Grocery-Bud/src/List.js
@@ -10,6 +10,14 @@ const List = ({ items }) => {
         return (
           <article key={id} className='grocery-item'>
             <p className="title">{title}</p>
+            <div className="btn-container">
+              <button type='button' className='edit-btn'>
+                <FaEdit />
+              </button>
+              <button type='button' className='delete-btn'>
+                <FaTrash />
+              </button>
+            </div>
           </article>
         )
       })}
