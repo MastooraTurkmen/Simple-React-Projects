71ca785bd5755be8414b8043e9bdb9777f12d92c
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-13 13:19:16 +0430

    adds spaces

--- 10-Grocery-Bud/src/List.js
+++ 10-Grocery-Bud/src/List.js
@@ -3,6 +3,7 @@ import { FaEdit, FaTrash } from 'react-icons/fa'
 
 
 const List = ({ items, removeItem, editItem }) => {
+  
   return (
     <div className="grocery-list">
       {items.map((item) => {
