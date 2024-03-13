7e5b1c39587d3a530a166ee4472c82592e9b838a
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-13 14:55:05 +0430

    adds navbar

--- 11-navbar/src/Navbar.js
+++ 11-navbar/src/Navbar.js
@@ -1,10 +1,56 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
+import { FaBars, FaTwitter, FaFacebook, FaInstagram } from 'react-icons/fa'
 import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+  return (
+    <>
+      <nav>
+        <div className="nav-center">
+          <div className="nav-header">
+            <img src={logo} alt="logo" />
+            <button className="nav-toggle">
+              <FaBars />
+            </button>
+          </div>
+          <div className="links-container show-container">
+            <ul className="links">
+              <li>
+                <a href="#">home</a>
+              </li>
+              <li>
+                <a href="#">about</a>
+              </li>
+              <li>
+                <a href="#">contact</a>
+              </li>
+              <li>
+                <a href="#">products</a>
+              </li>
+            </ul>
+          </div>
+          <ul className="social-icons">
+            <li>
+              <a target='_blank' href="https://www.twitter.com">
+                <FaTwitter />
+              </a>
+            </li>
+            <li>
+              <a target='_blank' href="https://www.facebook.com">
+                <FaFacebook />
+              </a>
+            </li>
+            <li>
+              <a target='_blank' href="https://www.instagram.com">
+                <FaInstagram />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  )
 }
 
 export default Navbar
