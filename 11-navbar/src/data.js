9f1510aa54aa1dc070d7a168755a34c5cec0beb2
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-13 15:36:31 +0430

    adds socail

--- 11-navbar/src/data.js
+++ 11-navbar/src/data.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FaBehance, FaFacebook, FaLinkedin, FaTwitter } from 'react-icons/fa'
+
 export const links = [
   {
     id: 1,
@@ -31,7 +32,7 @@ export const links = [
 export const social = [
   {
     id: 1,
-    url: 'https://www.twitter.com',
+    url: 'https://www.facebook.com',
     icon: <FaFacebook />,
   },
   {
@@ -41,12 +42,12 @@ export const social = [
   },
   {
     id: 3,
-    url: 'https://www.twitter.com',
+    url: 'https://www.linkedin.com',
     icon: <FaLinkedin />,
   },
   {
     id: 4,
-    url: 'https://www.twitter.com',
+    url: 'https://www.behance.com',
     icon: <FaBehance />,
   },
 ]
