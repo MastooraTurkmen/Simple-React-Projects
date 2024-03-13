af7c407f1c9ae1d5732331a2929d10467ab14586
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-13 14:58:08 +0430

    adds discord

--- 11-navbar/src/Navbar.js
+++ 11-navbar/src/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter, FaFacebook, FaInstagram } from 'react-icons/fa'
+import { FaBars, FaTwitter, FaFacebook, FaInstagram, FaDiscord } from 'react-icons/fa'
 import { links, social } from './data'
 import logo from './logo.svg'
 
@@ -44,6 +44,11 @@ const Navbar = () => {
             <li>
               <a target='_blank' href="https://www.instagram.com">
                 <FaInstagram />
+              </a>
+            </li>
+            <li>
+              <a target='_blank' href="https://www.instagram.com">
+                <FaDiscord />
               </a>
             </li>
           </ul>
