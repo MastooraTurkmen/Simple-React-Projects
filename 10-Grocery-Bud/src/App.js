171d3132fb7e7e1421c41d728c4c39538a2fad1f
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-13 13:07:21 +0430

    getLocalStrorageItem

--- 10-Grocery-Bud/src/App.js
+++ 10-Grocery-Bud/src/App.js
@@ -15,7 +15,7 @@ const getLocalStrorageItem = () => {
 
 function App() {
   const [name, setName] = useState('');
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStrorageItem());
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null)
   const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
