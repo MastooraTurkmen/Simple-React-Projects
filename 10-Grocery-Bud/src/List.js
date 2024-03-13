9f52d6f3d80342734e3038e5d465d99d9f14ecfb
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-13 13:26:08 +0430

    styling list

--- 10-Grocery-Bud/src/List.js
+++ 10-Grocery-Bud/src/List.js
@@ -3,7 +3,7 @@ import { FaEdit, FaTrash } from 'react-icons/fa'
 
 
 const List = ({ items, removeItem, editItem }) => {
-  
+
   return (
     <div className="grocery-list">
       {items.map((item) => {
@@ -12,10 +12,18 @@ const List = ({ items, removeItem, editItem }) => {
           <article key={id} className='grocery-item'>
             <p className="title">{title}</p>
             <div className="btn-container">
-              <button type='button' className='edit-btn' onClick={() => editItem(id)}>
+              <button
+                type='button'
+                className='edit-btn'
+                onClick={() => editItem(id)}
+              >
                 <FaEdit />
               </button>
-              <button type='button' className='delete-btn' onClick={() => removeItem(id)}>
+              <button
+                type='button'
+                className='delete-btn'
+                onClick={() => removeItem(id)}
+              >
                 <FaTrash />
               </button>
             </div>
