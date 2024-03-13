fb2cdc364924199c8a07852fbcca3b60a502064e
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-13 11:20:11 +0430

    remove alert

--- 10-Grocery-Bud/src/Alert.js
+++ 10-Grocery-Bud/src/Alert.js
@@ -1,6 +1,14 @@
 import React, { useEffect } from 'react'
 
-const Alert = ({ type, msg }) => {
+const Alert = ({ type, msg, removeAlert }) => {
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      removeAlert()
+    }, 3000)
+    return () => clearTimeout(timeOut)
+  }, [])
+
   return <p className={`alert alert-${type}`}>{msg}</p>
 }
 
