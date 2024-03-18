de409e3b01c5763f77dad598b237b69dd54ade69
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-18 10:12:12 +0430

    set up sidebar

--- 12-Sidebar-modal/src/Sidebar.js
+++ 12-Sidebar-modal/src/Sidebar.js
@@ -4,7 +4,29 @@ import { FaTimes } from 'react-icons/fa'
 import { social, links } from './data'
 
 const Sidebar = () => {
-  return <h2>sidebar</h2>
+  return (
+    <aside className={`sidebar show-sidebar`}>
+      <div className="sidebar-header">
+        <img src={logo} className='logo' alt="coding addict" />
+        <button className='close-btn'>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className='links'>
+        {links.map((link) => {
+          const { id, url, text, icon } = link
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+    </aside>
+  )
 }
 
 export default Sidebar
