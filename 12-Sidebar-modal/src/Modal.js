6ee25b7446121786f2671679bdeafe0ac532424a
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-18 10:07:40 +0430

    set up modal

--- 12-Sidebar-modal/src/Modal.js
+++ 12-Sidebar-modal/src/Modal.js
@@ -3,7 +3,7 @@ import { FaTimes } from 'react-icons/fa'
 
 const Modal = () => {
   return (
-    <div className={`modal-overlay show-modal`}>
+    <div className={`modal-overlay`}>
       <div className="modal-container">
         <h3>modal content</h3>
         <button className='close-modal-btn'>
