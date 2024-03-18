3a7b262e54fc359f66f5406d08bd3d0461323ea3
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-18 10:03:07 +0430

    set up modal

--- 12-Sidebar-modal/src/App.js
+++ 12-Sidebar-modal/src/App.js
@@ -2,10 +2,11 @@ import React from 'react'
 import Modal from './Modal'
 import Sidebar from './Sidebar'
 import Home from './Home'
+
 function App() {
   return (
     <>
-      <h2>modal-sidebar project setup</h2>
+      <Home />
     </>
   )
 }
