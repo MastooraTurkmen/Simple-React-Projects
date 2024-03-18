349ef311d23e810b6ead235ade688252045a17f9
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-18 10:43:17 +0430

    import useGlobalContext

--- 12-Sidebar-modal/src/Home.js
+++ 12-Sidebar-modal/src/Home.js
@@ -1,7 +1,10 @@
 import React, { useContext } from 'react'
 import { FaBars } from 'react-icons/fa'
+import { AppContext, useGlobalContext } from './context'
 
 const Home = () => {
+  const data = useGlobalContext()
+
   return <main>
     <button className='sidebar-toggle'>
       <FaBars />
