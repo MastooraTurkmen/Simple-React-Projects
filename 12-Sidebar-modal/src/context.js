6f9a5c18ed80a46d89a4204560e0891cccb1f185
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-18 10:28:40 +0430

    create context

--- 12-Sidebar-modal/src/context.js
+++ 12-Sidebar-modal/src/context.js
@@ -3,3 +3,13 @@ import React, { useState, useContext } from 'react'
 
 const AppContext = React.createContext();
 
+const AppProvider = ({ children }) => {
+    return (
+        <AppContext.Provider value={'hello worlddddd'}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+
+export default { AppContext, AppProvider }
