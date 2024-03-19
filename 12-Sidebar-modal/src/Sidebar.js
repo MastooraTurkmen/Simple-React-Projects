ca62008c3b24d5221b02b83a9c4012d15391e305
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-19 13:00:01 +0430

    set up sidebar closing and opening

--- 12-Sidebar-modal/src/Sidebar.js
+++ 12-Sidebar-modal/src/Sidebar.js
@@ -2,13 +2,17 @@ import React from 'react'
 import logo from './logo.svg'
 import { FaTimes } from 'react-icons/fa'
 import { social, links } from './data'
+import { useGlobalContext } from './context'
+
 
 const Sidebar = () => {
+  const { isSidebarOpen, closeSidebar } = useGlobalContext()
+
   return (
-    <aside className={`sidebar show-sidebar`}>
+    <aside className={`${isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'}`}>
       <div className="sidebar-header">
         <img src={logo} className='logo' alt="coding addict" />
-        <button className='close-btn'>
+        <button className='close-btn' onClick={closeSidebar}>
           <FaTimes />
         </button>
       </div>
