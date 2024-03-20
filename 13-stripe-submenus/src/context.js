a46d7376acd15be3ddcbb714d82daf9caf1b8b2d
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-20 10:09:45 +0430

    set up AppContext

--- 13-stripe-submenus/src/context.js
+++ 13-stripe-submenus/src/context.js
@@ -1,2 +1,18 @@
 import React, { useState, useContext } from 'react'
 import sublinks from './data'
+
+const AppContext = React.createContext();
+
+const AppProvidor = ({ children }) => {
+    const [isSidebar, setIsSidebar] = useState(false);
+    const [isModalbar, setIsModalbar] = useState(false);
+
+    return (
+        <AppContext.Provider value={{}}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+
+export { AppProvidor }
