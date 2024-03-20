6eaee9678e0059ad69e7d2788386b4a32f26b155
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-20 13:03:54 +0430

    display submenu

--- 13-stripe-submenus/src/Submenu.js
+++ 13-stripe-submenus/src/Submenu.js
@@ -1,7 +1,14 @@
 import React, { useState, useRef, useEffect } from 'react'
+import { useGlobalContext } from './context'
 
 const Submenu = () => {
-  return <h2>submenu component</h2>
+  const { isSubmenu } = useGlobalContext();
+
+  return (
+    <aside className={`${isSubmenu ? 'submenu show' : 'submenu'}`}>
+      submenu
+    </aside>
+  )
 }
 
 export default Submenu
