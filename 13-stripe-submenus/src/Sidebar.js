cd690f0ca99057a633e560fcae59d1d8cd003030
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-20 12:27:53 +0430

    set up sidebar

--- 13-stripe-submenus/src/Sidebar.js
+++ 13-stripe-submenus/src/Sidebar.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
 import sublinks from './data'
+import { useGlobalContext } from './context'
+
 
 const Sidebar = () => {
-  return <h2>sidebar component</h2>
+  const { isSidebar, closeSidebar } = useGlobalContext()
+
+  return (
+    <aside className={`${isSidebar ? 'sidebar-wrapper show' : 'sidebar-wrapper'}`}>
+      <div className="sidebar">
+        <button className="close-btn" onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+      </div>
+    </aside>
+  )
 }
 
 export default Sidebar
