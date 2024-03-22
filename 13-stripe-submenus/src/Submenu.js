04ef1b548c7b85508d54707aaa77698f90ce67ea
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-22 09:19:47 +0430

    set up links and page

--- 13-stripe-submenus/src/Submenu.js
+++ 13-stripe-submenus/src/Submenu.js
@@ -2,8 +2,9 @@ import React, { useState, useRef, useEffect } from 'react'
 import { useGlobalContext } from './context'
 
 const Submenu = () => {
-  const { isSubmenu, location } = useGlobalContext();
+  const { isSubmenu, location, page: { page, links } } = useGlobalContext();
   const container = useRef(null);
+  const [columns, setColumns] = useState('col-2');
 
   useEffect(() => {
     const submenu = container.current;
@@ -14,7 +15,18 @@ const Submenu = () => {
 
   return (
     <aside ref={container} className={`${isSubmenu ? 'submenu show' : 'submenu'}`}>
-      submenu
+      <h4>{page}</h4>
+      <div className={`submenu-center col-2`}>
+        {links.map((link, index) => {
+          const { label, icon, url } = link;
+          return (
+            <a key={index} href={url}>
+              {icon}
+              {label}
+            </a>
+          )
+        })}
+      </div>
     </aside>
   )
 }
