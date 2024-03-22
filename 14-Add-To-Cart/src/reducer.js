957d8fe909e4dc68758163436431ab13b898bc91
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-22 11:48:26 +0430

    increase and decrease item

--- 14-Add-To-Cart/src/reducer.js
+++ 14-Add-To-Cart/src/reducer.js
@@ -2,9 +2,33 @@ const reducer = (state, action) => {
     if (action.type === 'CLEAR_CART') {
         return { ...state, cart: [] }
     }
+
     if (action.type === 'REMOVE_CART') {
         return { ...state, cart: state.cart.filter((item) => item.id !== action.payload) }
     }
+
+    if (action.type === "INCREASE") {
+        let tempItem = state.cart.map((cartItem) => {
+            if (cartItem.id === action.payload) {
+                return { ...cartItem, amount: cartItem.amount + 1 }
+            }
+            return cartItem
+        })
+
+        return { ...state, cart: tempItem }
+    }
+
+    if (action.type === "DECREASE") {
+        let tempItem = state.cart.map((cartItem) => {
+            if (cartItem.id === action.payload) {
+                return { ...cartItem, amount: cartItem.amount - 1 }
+            }
+            return cartItem
+        })
+
+        return { ...state, cart: tempItem }
+    }
+
     return state
 }
 
