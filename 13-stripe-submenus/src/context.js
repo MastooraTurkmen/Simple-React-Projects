5e4c20ccce7a008a7985652c838b9a769524e1b7
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-22 09:36:49 +0430

    set up

--- 13-stripe-submenus/src/context.js
+++ 13-stripe-submenus/src/context.js
@@ -4,27 +4,28 @@ import sublinks from './data'
 const AppContext = React.createContext();
 
 export const AppProvider = ({ children }) => {
+
     const [isSidebar, setIsSidebar] = useState(false);
     const [isSubmenu, setIsSubmenu] = useState(false);
-    const [location, setLocation] = useState({})
-    const [page, setPage] = useState({ page: '', links: [] })
+    const [location, setLocation] = useState({});
+    const [page, setPage] = useState({ page: '', links: [] });
 
     const openSidebar = () => {
         setIsSidebar(true)
-    }
+    };
     const closeSidebar = () => {
         setIsSidebar(false)
-    }
+    };
 
     const openSubmenu = (text, coordinator) => {
         const page = sublinks.find((link) => link.page === text);
         setPage(page)
         setIsSubmenu(true)
         setLocation(coordinator)
-    }
+    };
     const closeSubmenu = () => {
         setIsSubmenu(false)
-    }
+    };
 
     return (
         <AppContext.Provider value={{
