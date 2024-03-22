4a111787b0e348e611e6fe4ac25fb1238ac4af6a
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-22 09:24:33 +0430

    set up columns

--- 13-stripe-submenus/src/Submenu.js
+++ 13-stripe-submenus/src/Submenu.js
@@ -7,16 +7,28 @@ const Submenu = () => {
   const [columns, setColumns] = useState('col-2');
 
   useEffect(() => {
+    setColumns('col-2')
     const submenu = container.current;
     const { center, bottom } = location;
     submenu.style.top = `${bottom}px`;
     submenu.style.left = `${center}px`;
-  }, [location])
+
+    if (links.length === 4) {
+      setColumns('col-4')
+    }
+
+    if (links.length === 3) {
+      setColumns('col-3')
+    }
+
+  }, [location]);
+
+
 
   return (
     <aside ref={container} className={`${isSubmenu ? 'submenu show' : 'submenu'}`}>
       <h4>{page}</h4>
-      <div className={`submenu-center col-2`}>
+      <div className={`submenu-center ${columns}`}>
         {links.map((link, index) => {
           const { label, icon, url } = link;
           return (
