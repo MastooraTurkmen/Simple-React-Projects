d89515fae36fdbf8a703c52649074002ee42ce2e
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-03 09:43:03 +0430

    adds my username

--- 17-Pagination/src/useFetch.js
+++ 17-Pagination/src/useFetch.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import paginate from './utils'
-const url = 'https://api.github.com/users/john-smilga/followers?per_page=100'
+const url = 'https://api.github.com/users/MastooraTurkmen/followers?per_page=100'
 
 export const useFetch = () => {
   const [loading, setLoading] = useState(true)
