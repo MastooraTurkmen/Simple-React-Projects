afcfef94c13a0c893b915fffa2b268485d69a02e
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-03 10:38:34 +0430

    setFollowers in every page

--- 17-Pagination/src/App.js
+++ 17-Pagination/src/App.js
@@ -3,7 +3,15 @@ import { useFetch } from './useFetch'
 import Follower from './Follower'
 
 function App() {
-  const { loading, data } = useFetch()
+  const { loading, data } = useFetch();
+  const [page, setPage] = useState(0);
+  const [followers, setFollowers] = useState([]);
+
+  useEffect(() => {
+    if (loading) return;
+    setFollowers(data[page])
+  }, [followers])
+
   return (
     <main>
       <div className="section-title">
@@ -12,7 +20,7 @@ function App() {
       </div>
       <section className="followers">
         <div className="container">
-          {data.map((follower) => {
+          {followers.map((follower) => {
             return (
               <Follower key={follower.id} {...follower} />
             )
