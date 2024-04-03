b61478002bf416837b110ee2065a2e765c221454
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-03 09:38:50 +0430

    set up data

--- 17-Pagination/src/App.js
+++ 17-Pagination/src/App.js
@@ -1,8 +1,26 @@
 import React, { useState, useEffect } from 'react'
 import { useFetch } from './useFetch'
 import Follower from './Follower'
+
 function App() {
-  return <h2>pagination starter</h2>
+  const { loading, data } = useFetch()
+  return (
+    <main>
+      <div className="section-title">
+        <h2>{loading ? 'loading...' : 'pagination'}</h2>
+        <div className="underline"></div>
+      </div>
+      <section className="followers">
+        <div className="container">
+          {data.map((follower) => {
+            return (
+              <Follower key={follower.id} {...follower} />
+            )
+          })}
+        </div>
+      </section>
+    </main>
+  )
 }
 
 export default App
