ad458d9fee947d4f36ec53b63710257246b4867a
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-03 10:35:34 +0430

    target blank

--- 17-Pagination/src/Follower.js
+++ 17-Pagination/src/Follower.js
@@ -5,7 +5,7 @@ const Follower = ({ html_url, avatar_url, login }) => {
     <article className="card">
       <img src={avatar_url} alt={login} />
       <h4>{login}</h4>
-      <a href={html_url} className='btn'>view profile</a>
+      <a target='_blank' href={html_url} className='btn'>view profile</a>
     </article>
   )
 }
