c72cddba0cba0b6ecf53ee6998405714cacd9203
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-03 13:19:54 +0430

    set up prev and next pages

--- 17-Pagination/src/App.js
+++ 17-Pagination/src/App.js
@@ -16,6 +16,26 @@ function App() {
     setPage(index)
   }
 
+  const nextBtn = () => {
+    setPage((oldPage) => {
+      let newPage = oldPage + 1;
+      if (newPage > data.length - 1) {
+        newPage = 0;
+      }
+      return newPage
+    })
+  }
+
+  const prevBtn = () => {
+    setPage((oldPage) => {
+      let prevPage = oldPage - 1;
+      if (prevPage < 0) {
+        prevPage = data.length - 1;
+      }
+      return prevPage
+    })
+  }
+
   return (
     <main>
       <div className="section-title">
@@ -31,13 +51,15 @@ function App() {
           })}
         </div>
         {!loading && <div className="btn-container">
+          <button className='prev-btn' onClick={prevBtn}>prev</button>
           {data.map((item, index) => {
             return <button
               className={`page-btn ${index === page ? 'active-btn' : null}`}
               onClick={() => handleButton(index)}
 
-            >{index}</button>
+            >{index + 1}</button>
           })}
+          <button className='next-btn' onClick={nextBtn}>next</button>
         </div>
         }
       </section>
