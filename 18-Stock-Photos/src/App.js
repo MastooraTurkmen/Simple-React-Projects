6d7230661e141e2fe0c2f2a7cc841c182681a321
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-04 20:41:53 +0430

    get link

--- 18-Stock-Photos/src/App.js
+++ 18-Stock-Photos/src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { FaSearch } from 'react-icons/fa'
 import Photo from './Photo'
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
 const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
 
@@ -11,7 +12,7 @@ function App() {
 
   const fetchData = async () => {
     let url;
-    url = `${mainUrl}?client_id=fJIEIAp7KvuevajNm9EmtYa-kIQSzKJn6PhLxUi9E84`
+    url = `${mainUrl}${clientID }`
     try {
       const response = await fetch(url)
       const data = await response.json()
