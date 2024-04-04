5d60a9661bd37e251d6b42c053f74c15e2cd8b50
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-04 22:38:06 +0430

    set up photo

--- 18-Stock-Photos/src/Photo.js
+++ 18-Stock-Photos/src/Photo.js
@@ -1,7 +1,19 @@
 import React from 'react'
 
-const Photo = () => {
-  return <h2>photo component</h2>
+const Photo = ({ alt_description, likes, urls: { regular }, user: { name, portfolio_url, portfolio_image: { medium } } }) => {
+  return (
+    <article className="photo">
+      <img src={regular} alt={alt_description} />
+      <div className="photo-info">
+        <div>
+          <h4>{name}</h4>
+          <p>{likes} likes</p>
+        </div>
+        <a href={portfolio_url}>
+          <img src={medium} alt={name} className='user-img' />
+        </a>
+      </div>
+    </article>)
 }
 
 export default Photo
