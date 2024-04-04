fd8b2ca26a6eecb1f477f71a05a93f22572620b0
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-04 22:37:59 +0430

    set up phot

--- 18-Stock-Photos/src/App.js
+++ 18-Stock-Photos/src/App.js
@@ -8,7 +8,7 @@ const searchUrl = `https://api.unsplash.com/search/photos/`
 
 function App() {
   const [loading, setLoading] = useState(false);
-  const [photo, setPhoto] = useState([]);
+  const [photos, setPhotos] = useState([]);
 
   const fetchData = async () => {
     setLoading(true)
@@ -17,6 +17,8 @@ function App() {
     try {
       const response = await fetch(url)
       const data = await response.json()
+      setPhotos(data)
+      setLoading(false)
       console.log(data);
     } catch (error) {
       console.log(error);
@@ -28,7 +30,30 @@ function App() {
     fetchData()
   }, [])
 
-  return <h2>stock photos starter</h2>
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  return (
+    <main>
+      <section className="search">
+        <form className="search-form">
+          <input type="text" placeholder='search' className='form-input' />
+          <button className="submit-btn" onClick={handleSubmit}>
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+      <section className="photos">
+        <div className="photos-center">
+          {photos.map((image) => {
+            <Photo key={image.id} {...image} />
+          })}
+        </div>
+      </section>
+      {loading && <h2 className='loading'>Loading...</h2>}
+    </main>
+  )
 }
 
 export default App
