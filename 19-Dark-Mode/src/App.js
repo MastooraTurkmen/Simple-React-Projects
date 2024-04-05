a0b715d31d3c8aff282c63aafde9a2392649e991
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-05 21:53:54 +0430

    set up article

--- 19-Dark-Mode/src/App.js
+++ 19-Dark-Mode/src/App.js
@@ -3,7 +3,23 @@ import data from './data'
 import Article from './Article'
 
 function App() {
-  return <h2>dark mode starter</h2>
+  return (
+    <main>
+      <nav>
+        <div className="nav-center">
+          <h1>overreacted</h1>
+          <button className="btn">
+            toggle
+          </button>
+        </div>
+      </nav>
+      <section className="articles">
+        {data.map((articles) => {
+          return <Article key={articles.id} {...articles} />
+        })}
+      </section>
+    </main>
+  )
 }
 
 export default App
