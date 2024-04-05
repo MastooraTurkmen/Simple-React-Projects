f015f7e642e7624eac770afce45fed3519988e5b
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-05 21:53:56 +0430

    set up article

--- 19-Dark-Mode/src/Article.js
+++ 19-Dark-Mode/src/Article.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import moment from 'moment'
-const Article = () => {
-  return <h2>article component</h2>
+
+const Article = ({ title, snippet, date, length }) => {
+  return (
+    <article className='post'>
+      <h2>{title}</h2>
+      <div className="post-info">
+        <span>date</span>
+        <span>{length} min read</span>
+      </div>
+      <p>{snippet}</p>
+    </article>
+  )
 }
 
 export default Article
